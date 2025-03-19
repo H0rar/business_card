38f1bb87cb3e275e5b76e091019a64f2ff489b74
Dušan Rohár <dusanrohar@gmail.com>
2025-03-19 10:36:36 +0100

    Added Headers and styles

--- business-card-app/src/components/App.js
+++ business-card-app/src/components/App.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React  from "react";
+import Header from "./Header";
 
 const App = () => {
-  return <div className='App'>nazdar kokot</div>
+  return <div className="App">
+    <Header Dark={true}>Professional Business Card Maker</Header>
+  </div>
 }
 
-export default App
+export default App;
--- business-card-app/src/tests/App.test.js
+++ business-card-app/src/tests/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from "../components/App";
 
-test('renders servus chuju text', () => {
+test('renders servus text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/nazdar kokot/i);
+  const linkElement = screen.getByText(/nazdar /i);
   expect(linkElement).toBeInTheDocument();
 });
